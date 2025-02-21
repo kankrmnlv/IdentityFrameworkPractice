db5abb76611fcd121fad790b49b3e4a7f1ffc277
kankrmnlv <kkanigaming@gmail.com>
2025-02-21 19:00:47 +0800

    began with edit, done with httpget

--- IdentityFrameworkPractice/Controllers/UserController.cs
+++ IdentityFrameworkPractice/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using IdentityFrameworkPractice.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace IdentityFrameworkPractice.Controllers
 {
@@ -34,6 +35,31 @@ namespace IdentityFrameworkPractice.Controllers
             }
 
             return View(userList);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(string userId)
+        {
+            var user = _db.AppUser.FirstOrDefault(u => u.Id ==  userId);
+            if(user == null)
+            {
+                return NotFound();
+            }
+
+            var userRole = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
+            var role = userRole.FirstOrDefault(u => u.UserId == user.Id);
+            if(role != null)
+            {
+                user.RoleId = roles.FirstOrDefault(u => u.Id == role.RoleId).Id;
+            }
+            user.RoleList = _db.Roles.Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id
+            });
+
+            return View(user);
         }
     }
 }
